8e42a232dd82a11f2b3d0ad1e9b88b60054e3d68
jobtalle <jobtalle@hotmail.com>
2014-11-09 01:03:07 +0100

    Committed source files

---
+++ include/ccNoise/ccNoise.h
@@ -0,0 +1,21 @@
+//__________________________________________________________________________________//
+//                                                                                  //
+//                               _   _       _                                      //
+//                              | \ | |     (_)                                     //
+//                       ___ ___|  \| | ___  _ ___  ___                             //
+//                      / __/ __| . ` |/ _ \| / __|/ _ \                            //
+//                     | (_| (__| |\  | (_) | \__ \  __/                            //
+//                      \___\___|_| \_|\___/|_|___/\___| 1.0                        //
+//                                                                                  //
+//                                                                                  //
+//             Copyright (C) 2014 \ Job Talle (job@ccore.org)                       //
+//__________________________________________________________________________________//
+//                                                                                  //
+//      This program is free software: you can redistribute it and/or modify        //
+//      it under the terms of the 3-clause BSD license.                             //
+//                                                                                  //
+//      You should have received a copy of the 3-clause BSD License along with      //
+//      this program. If not, see <http://opensource.org/licenses/>.                //
+//__________________________________________________________________________________//
+
+#pragma once
